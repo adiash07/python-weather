f4c1c54bb7ffccfb54da43d3709bdb5902112ab7
adiash07 <152286909+adiash07@users.noreply.github.com>
2023-11-29 17:31:31 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -8,8 +8,8 @@ load_dotenv()
 
 def get_current_weather(city='Hyderabad'):
 
-    request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={
-        os.getenv("API_KEY")}&q={city}&units=imperial'
+    request_url = f"http://api.openweathermap.org/data/2.5/weather?appid={
+        os.getenv('API_KEY')}&q={city}&units=imperial"
 
     weather_data = requests.get(request_url).json()
 
